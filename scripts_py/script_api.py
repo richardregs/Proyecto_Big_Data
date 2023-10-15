989bfd197cd0f6c739af098d916f57093615da9e
Richard Gomez <richardregs96@gmail.com>
2023-10-15 02:22:59 -0500

    add docker

--- scripts_py/script_api.py
+++ scripts_py/script_api.py
@@ -8,13 +8,13 @@ import pandas as pd
 
 
 PATH_FILE = r"C:\Users\Usuario\Downloads\Calendario Agricola.xlsx"
-conn = http.client.HTTPConnection("127.0.0.1", 8000)
+conn = http.client.HTTPConnection("34.28.131.194", 8000)
 
 df = pd.read_excel(PATH_FILE, sheet_name="Calendario Agrícola")
 df_dict = df.to_dict('records')
 
 payload = json.dumps({
-    "file_name": "Calendario Agricola",
+    "file_name": "Calendario Agricolav2",
     "data": df_dict
     })
 headers = {
