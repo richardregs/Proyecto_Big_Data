d2166c8b6535c33af2bd430480d5a28d65cb944f
Richard Gomez <richardregs96@gmail.com>
2023-10-15 18:45:16 -0500

    fix model

--- API/src/model/User.py
+++ API/src/model/User.py
@@ -14,7 +14,7 @@ class User(BaseModel):
     username: Union[str, None] = None
     full_name: Union[str, None] = None
     email: Union[str, None] = None
-    disabled: Union[str, None] = None
+    disabled: Union[bool, None] = None
 
 
 class UserInDB(User):
